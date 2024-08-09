cb127c3880029f3a1e3da8487feae4226b9b5cec
Sewell Stephens <sewellstephens104@gmail.com>
2024-08-08 21:51:14 -0500

    chore: Add thank you page component

---
+++ app/thanks/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+    return (
+        <div className="m-auto max-w-4xl">
+        <Header />
+        <div className='mt-4'>
+            <h1 className="text-2xl font-bold">Thank you for subscribing!</h1>
+            <p>Thank you for subscribing to my newsletter. You will receive updates on my latest articles and projects.</p>
+        </div>
+        </div>
+    )
+}
